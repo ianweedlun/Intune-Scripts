4a0d8159cb3bad90ffe756b017ed5cd30c61dadc
weedwums <46026693+weedwums@users.noreply.github.com>
2022-10-04 15:48:22 -0400

    Uploading Disable ESP script
    This is a script to be run on any devices where you do NOT want the ESP to show for users & devices.

---
+++ DisablingESP.ps1
@@ -0,0 +1,45 @@
+function Test-RegistryValue {
+    param (
+     [parameter(Mandatory=$true)]
+     [ValidateNotNullOrEmpty()]$Path,
+    
+    [parameter(Mandatory=$true)]
+     [ValidateNotNullOrEmpty()]$Value
+    )
+    
+    try {
+        Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null
+        return $true
+    }catch {
+        return $false
+    }
+}
+
+$key = 'HKLM:SOFTWARE\Microsoft\Enrollments\'
+$keys = (Get-ChildItem $key)
+foreach($subkey in $keys) {
+    $children =(Get-ChildItem($key + $subkey.PSChildName))
+    foreach($child in $children) {
+    if($child.PSChildName -eq 'FirstSync')    {
+        $location =($key + $subkey.PSChildName)
+        }
+    }
+}
+
+if(Test-RegistryValue -Path "$location\FirstSync" -value SkipUserStatusPage) {
+    if((Get-ItemProperty -Path "$location\FirstSync" | Select-Object -ExpandProperty SkipUserStatusPage) -ne 1){
+        Set-ItemProperty -path "$location\FirstSync" -name SkipUserStatusPage -value 1
+    }
+}
+else {    
+    New-ItemProperty -Path "$location\FirstSync" -name SkipUserStatusPage -value 1 -PropertyType Dword
+}
+
+if(Test-RegistryValue -Path "$location\FirstSync" -value SkipDeviceStatusPage) {
+    if((Get-ItemProperty -Path "$location\FirstSync" | Select-Object -ExpandProperty SkipDeviceStatusPage) -ne 1){
+        Set-ItemProperty -path "$location\FirstSync" -name SkipDeviceStatusPage -value 1
+    }
+}
+else {    
+    New-ItemProperty -Path "$location\FirstSync" -name SkipDeviceStatusPage -value 1 -PropertyType Dword
+}
