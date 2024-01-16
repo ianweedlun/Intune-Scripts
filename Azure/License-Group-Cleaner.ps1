84184e9e49560b43d489018e1aeb1f796ab9d893
Ian Weedlun <46026693+ianweedlun@users.noreply.github.com>
2024-01-16 17:21:17 -0500

    Works now, finalish version

--- Azure/License-Group-Cleaner.ps1
+++ Azure/License-Group-Cleaner.ps1
@@ -12,7 +12,7 @@ function GroupAddBySKU {
 
     $groupId = Get-MgGroup -Filter "DisplayName eq '$groupName'" | select Id
 
-    Write-Host "Checking that all users assigned " $skuId " are members of " $groupName "."
+    Write-Host "Checking that all users assigned" $skuId "are members of" $groupName"."
 
     ForEach ($user in $allUsers) {
         try {
@@ -20,8 +20,7 @@ function GroupAddBySKU {
             New-MgGroupMember -Group $groupId.Id -DirectoryObjectId $user.Id -ErrorAction stop  
             Write-Host $user.UserPrincipalName "has been added to " $groupName                
         }
-        catch {
-            Write-Host $user.UserPrincipalName "has not been changed."
+        catch {            
         }
     }
 }
@@ -35,7 +34,7 @@ function RemoveDirectLicenseAssignments {
 
     $allUsers = Get-MgUser -Filter "assignedLicenses/any(x:x/skuId eq $($skuId) )"
 
-    Write-Host "Checking for SKU" $skuId " directly assigned to users."
+    Write-Host "Checking for SKU" $skuId "directly assigned to users."
 
     foreach ($user in $allUsers) {
     
@@ -49,8 +48,7 @@ function RemoveDirectLicenseAssignments {
                     Set-MgUserLicense -UserId $user.Id -RemoveLicenses @($license) -AddLicenses @{} -ErrorAction stop
                     Write-Host $user.UserPrincipalName "has had" $skuId "removed."
                 }
-                catch {
-                    Write-Host $user.UserPrincipalName "has not been changed."
+                catch {                    
                 }
             }
         }
@@ -59,36 +57,44 @@ function RemoveDirectLicenseAssignments {
 
 # Connect to MS Graph with required permissions
 
-Connect-MgGraph -Scopes User.ReadWrite.All, Organization.Read.All, Group.ReadWrite.All
+Connect-MgGraph -Scopes User.ReadWrite.All, Organization.Read.All, Group.ReadWrite.All -NoWelcome
 
 # Define table of products and their corresponding group name.
 
+# Get-MgSubscribedSku | Select -Property Sku*, ConsumedUnits -ExpandProperty PrepaidUnits | Format-List
+
 $products = @{
-    O365_BUSINESS_ESSENTIALS = 'M365 License - Business Basic'
+    SPB                      = "M365 License - Business Premium"
+    O365_BUSINESS_PREMIUM    = "M365 License - Business Standard"
+    O365_BUSINESS_ESSENTIALS = "M365 License - Business Basic"
+    EXCHANGESTANDARD         = "M365 License - Exchange P1"
+    EXCHANGEENTERPRISE       = "M365 License - Exchange P2"
+    SPE_F1                   = "M365 License - F3"
+    VISIOCLIENT              = "M365 License - Visio Plan 2"
+    PROJECTPROFESSIONAL      = "M365 License - Project Plan 3"
 }
 
 foreach ($product in $products.keys) {
 
-    # Find license SKU  
+    # Find license SKU
 
+    Write-Host "Searching for" $product"."
+    
     $SKU = Get-MgSubscribedSku -All | Where SkuPartNumber -eq $product
-    $SKU = $SKU.skuId
 
-    # Find group name
-
-    $group = $products[$product]
-
-    # Add all licensees to the proper corresponding group
-
-    GroupAddBySKU -skuId $SKU -groupName $group
-    Write-Host "Group memberships corrected, moving onto removing direct assignments."
-
-    Sleep 3
-
-    # Remove same license if directly assigned to users
-
-    RemoveDirectLicenseAssignments -skuId $SKU
-
+    if ($SKU) {    
+        $SKU = $SKU.skuId    
+        $group = $products[$product]
+        # Add all licensees to the proper corresponding group        
+        GroupAddBySKU -skuId $SKU -groupName $group    
+        Write-Host "Group memberships corrected, moving onto removing direct assignments."
+        Sleep 3
+        # Remove same license if directly assigned to users
+        RemoveDirectLicenseAssignments -skuId $SKU  
+    }
+    else {
+        Write-Host $product "not found."
+    }    
 }
 
 Disconnect-MgGraph
