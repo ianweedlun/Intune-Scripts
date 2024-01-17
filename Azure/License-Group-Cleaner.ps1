ff787569ac62dcba0e119ec340c8b87ccc2c1ff0
Ian Weedlun <46026693+ianweedlun@users.noreply.github.com>
2024-01-17 16:44:48 -0500

    Formatting + fixing group search (was unable to find longer group names before)

--- Azure/License-Group-Cleaner.ps1
+++ Azure/License-Group-Cleaner.ps1
@@ -10,16 +10,16 @@ function GroupAddBySKU {
 
     # Find group ID
 
-    $groupId = Get-MgGroup -Filter "DisplayName eq '$groupName'" | select Id
+    $groupId = (Get-MgGroup | Where-Object { $_.DisplayName -eq $groupName }).Id
 
     Write-Host "Checking that all users assigned" $skuId "are members of" $groupName"."
 
     ForEach ($user in $allUsers) {
         # need an if users group list is missing group name to replace the try/catch
         $groups = Get-MgUserMemberOf -UserId $user.Id
-        if ($groups.Id -notcontains $groupId.Id) {
+        if ($groups.Id -notcontains $groupId) {
             # Try to add the user to the new group
-            New-MgGroupMember -Group $groupId.Id -DirectoryObjectId $user.Id
+            New-MgGroupMember -Group $groupId -DirectoryObjectId $user.Id
             Write-Host $user.UserPrincipalName "has been added to" $groupName
         }
         else {
@@ -34,7 +34,7 @@ function RemoveDirectLicenseAssignments {
 
     # Get all users with SKU
 
-    $users = Get-MgUser -All -Property AssignedLicenses, LicenseAssignmentStates, DisplayName, Id | Select-Object DisplayName, AssignedLicenses, Id -ExpandProperty LicenseAssignmentStates | Select-Object DisplayName, AssignedByGroup, Id, SkuId | Where-Object { $_.SkuId -eq $skuId} | Where-Object { $_.AssignedByGroup -eq $null }
+    $users = Get-MgUser -All -Property AssignedLicenses, LicenseAssignmentStates, DisplayName, Id | Select-Object DisplayName, AssignedLicenses, Id -ExpandProperty LicenseAssignmentStates | Select-Object DisplayName, AssignedByGroup, Id, SkuId | Where-Object { $_.SkuId -eq $skuId } | Where-Object { $_.AssignedByGroup -eq $null }
 
     Write-Host "Checking for SKU" $skuId "directly assigned to users."
 
@@ -44,47 +44,54 @@ function RemoveDirectLicenseAssignments {
     }
 }
 
-    # Connect to MS Graph with required permissions
+# Connect to MS Graph with required permissions
 
-    Connect-MgGraph -Scopes User.ReadWrite.All, Organization.Read.All, Group.ReadWrite.All -NoWelcome
+Connect-MgGraph -Scopes User.ReadWrite.All, Organization.Read.All, Group.ReadWrite.All -NoWelcome
 
-    # Define table of products and their corresponding group name.
+# Define table of products and their corresponding group name.
 
-    # Get-MgSubscribedSku | Select -Property Sku*, ConsumedUnits -ExpandProperty PrepaidUnits | Format-List
+# Get-MgSubscribedSku | Select -Property Sku*, ConsumedUnits -ExpandProperty PrepaidUnits | Format-List
 
-    $products = @{
-        VISIOCLIENT              = "M365 License - Visio Plan 2"
-        EXCHANGEENTERPRISE       = "M365 License - Exchange P2"
-        SPB                      = "M365 License - Business Premium"
-        <#O365_BUSINESS_PREMIUM    = "M365 License - Business Standard"
-        O365_BUSINESS_ESSENTIALS = "M365 License - Business Basic"
-        EXCHANGESTANDARD         = "M365 License - Exchange P1"
-        SPE_F1                   = "M365 License - F3"
-        PROJECTPROFESSIONAL      = "M365 License - Project Plan 3"
-        MCOTEAMS_ESSENTIALS      = "M365 License - Business Premium + Teams Phone with Calling Plan (country zone 1 - US)"
-        MCOPSTN9                 = "M365 License - Business Premium + Microsoft Teams International Calling Plan (for SMB)"#>
+$products = @{
+    VISIOCLIENT               = "M365 License - Visio Plan 2"
+    EXCHANGEENTERPRISE        = "M365 License - Exchange P2"
+    SPB                       = "M365 License - Business Premium"
+    O365_BUSINESS_PREMIUM     = "M365 License - Business Standard"
+    O365_BUSINESS_ESSENTIALS  = "M365 License - Business Basic"
+    EXCHANGESTANDARD          = "M365 License - Exchange P1"
+    SPE_F1                    = "M365 License - F3"
+    PROJECTPROFESSIONAL       = "M365 License - Project Plan 3"
+    MCOTEAMS_ESSENTIALS       = "M365 License - Business Premium + Teams Phone with Calling Plan (country zone 1 - US)"
+    MCOPSTN9                  = "M365 License - Business Premium + Microsoft Teams International Calling Plan (for SMB)"
+}
+
+Write-Host "Launching M365 license group organizer`n" -ForegroundColor Green
+
+foreach ($product in $products.keys) {
+
+    # Find license SKU
+
+    "-" * 80 | Write-Host -ForegroundColor Green
+    Write-Host "Searching for" $products[$product]
+
+    $SKU = Get-MgSubscribedSku -All | Where SkuPartNumber -eq $product
+
+    if ($SKU) {
+        $SKU = $SKU.skuId
+        $group = $products[$product]
+        # Add all licensees to the proper corresponding group
+        GroupAddBySKU -skuId $SKU -groupName $group
+        Write-Host "Group memberships corrected, moving onto removing direct assignments."
+        # Remove same license if directly assigned to users
+        RemoveDirectLicenseAssignments -skuId $SKU
+        "-" * 80 | Write-Host -ForegroundColor Green
+        Write-Host "`n"
     }
-
-    foreach ($product in $products.keys) {
-
-        # Find license SKU
-
-        Write-Host "Searching for" $product"."
-
-        $SKU = Get-MgSubscribedSku -All | Where SkuPartNumber -eq $product
-
-        if ($SKU) {
-            $SKU = $SKU.skuId
-            $group = $products[$product]
-            # Add all licensees to the proper corresponding group
-            GroupAddBySKU -skuId $SKU -groupName $group
-            Write-Host "Group memberships corrected, moving onto removing direct assignments."
-            # Remove same license if directly assigned to users
-            RemoveDirectLicenseAssignments -skuId $SKU
-        }
-        else {
-            Write-Host $product "not found."
-        }
+    else {
+        Write-Host $product "subscription not found in this tenant" -ForegroundColor Red
+        "-" * 80 | Write-Host -ForegroundColor Green
+        Write-Host "`n"
     }
+}
 
-    # Disconnect-MgGraph
+# Disconnect-MgGraph
